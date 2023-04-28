29fc5f71e664c607adeba740c49dff9a07572288
edcan-dev <israel.da.queenie@gmail.com>
2023-04-27 23:44:21 -0600

    solo deploy

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const authBut = document.getElementById("auth_but").addEventListener("click",(e) => {
+
+
+    console.log("asu");
+    //e.preventDefault();
+
+    //const url = "http://localhost:8080/auth/azuread";
+
+       //Don't like hashes. Changing it back.
+
+});
+
+
